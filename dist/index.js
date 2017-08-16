53431832ee5f1772a667eb22de0c60992778f1d9
kudefe <kudefeg@gmail.com>
2017-08-16 18:44:06 -0300

    de nuevo

--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,7 @@ var _app2 = _interopRequireDefault(_app);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_app2.default.listen(3000, function (req, res) {
+_app2.default.listen(process.env.PORT || 3000, function (req, res) {
   console.log("funcionando");
 });
 //# sourceMappingURL=index.js.map
