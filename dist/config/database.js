eeb5a6b6ac932d879aac3636461c3d3a6c9a4846
kudefe <kudefeg@gmail.com>
2017-08-17 00:34:54 -0300

    de nuevo

--- dist/config/database.js
+++ dist/config/database.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  database: 'mongodb://kud:12345@ds019826.mlab.com:19826/gamedevdb',
+  database: 'mongodb://kud:12345@ds149373.mlab.com:49373/devmemoir',
   secret: 'misecreto'
 };
 //# sourceMappingURL=database.js.map
